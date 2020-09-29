b9d191ffc4fa2e48cbe6cf777c7ae0af8c29b952
Alexander Bowers <7319abowers@gmail.com>
2020-09-29 22:53:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 
 def square_array(array)
   square_array = []
-  array.each do |count|
-square_array << count = count * count
+  until square_array.length == array.length
+    array.each do |count|
+      square_array << count = count * count
+    end
   end
 end
